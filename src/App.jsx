08e767353637f00ebf56c3d198f3bc41b947bab8
SaimaS <saima.salonen@gmail.com>
2024-02-13 14:05:03 +0200

    07 tapahtumankäsittelijäfunktio määritetty suoraan onClick-määrittelyssä.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  const handleClick = () => {
-    console.log("clicked");
-  };
+  // const handleClick = () => {
+  //   console.log("clicked");
+  // };
 
   return (
     <>
       <div>{counter}</div>
-      <button onClick={handleClick}>plus</button>
+      <button onClick={() => console.log("clicked")}>plus</button>
     </>
   );
 };
