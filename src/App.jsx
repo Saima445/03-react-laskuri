59e61a3962a4ed8beb406f5c687144226127e874
SaimaS <saima.salonen@gmail.com>
2024-02-14 13:17:23 +0200

    10 eriytetty display ja napit omiksi komponenteiksi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import { useState } from "react";
 
+const Display = (props) => {
+  return (
+    <section>
+      <p>{props.counter}</p>
+    </section>
+  );
+};
+
+const Button = (props) => {
+  return (
+    <section>
+      <button onClick={props.handleClick}>{props.text}</button>
+    </section>
+  );
+};
+
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
 
-  const increaseByOne = () => setCounter(counter + 1);
+  const increaseByOne = () => {
+    console.log("increasing, value before:", counter);
+    setCounter(counter + 1);
+  };
 
-  const setToZero = () => setCounter(0);
+  const decreasByOne = () => {
+    console.log("decreasing, value before:", counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before:", counter);
+    setCounter(0);
+  };
 
   return (
     <>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreasByOne} text="minus" />
     </>
   );
 };
